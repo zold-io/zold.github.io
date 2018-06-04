81c203eec28203663e47ca2f213903afd212674a
rultor <me@rultor.com>
2018-06-04 09:42:18 +0000

    new web front

--- js/map.js
+++ js/map.js
@@ -22,25 +22,37 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-function put_marker_by_ip(map, coords, ip, port) {
-  $.getJSON('http://www.geoplugin.net/json.gp?ip=' + ip, function(json) {
-    var lat = parseFloat(json['geoplugin_latitude']), lon = parseFloat(json['geoplugin_longitude']);
-    console.log(ip + ' located at ' + lat + '/' + lon);
-    new google.maps.Marker({
-      position: { lat: lat, lng: lon },
-      map: map,
-      title: coords
-    });
-    console.log('Marker set for ' + coords + ' at ' + lat + '/' + lon);
-  }).fail(function() { console.log('Failed to find geo-location for ' + ip) });
+function init() {
+  var map = new google.maps.Map(
+    document.getElementById("map"),
+    {
+      center: new google.maps.LatLng(55.751244, 37.618423),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      zoom: 3
+    }
+  );
+  refresh('b2.zold.io:4096', map);
+};
+
+function refresh(host, map) {
+  $.getJSON('http://' + host + '/', function(data) {
+    $('#header').html(
+      'Version: ' + data['version'] + '<br/>' +
+      'Host: ' + data['score']['host'] + ':' + data['score']['port'] + '<br/>' +
+      'Score: ' + data['score']['value'] + '<br/>' +
+      'Wallets: ' + data['wallets']
+    );
+    refresh_list(host, map);
+    window.setTimeout(function () { refresh(host, map); }, 10000);
+  });
 }
 
-function put_marker_by_host(map, coords, host, port) {
-  $.getJSON('https://api.exana.io/dns/' + host + '/a', function(json) {
-    ip = $.grep(json['answer'], function (a, i) { return a['type'] == 'A'; })[0]['rdata'];
-    console.log('Host ' + host + ' resolved to ' + ip);
-    put_marker_by_ip(map, coords, ip, port);
-  }).fail(function() { console.log('Failed to find IP for ' + host) });
+function refresh_list(host, map) {
+  $.getJSON('http://' + host + '/remotes', function(data) {
+    var remotes = data['all'];
+    console.log(remotes.length + ' remote nodes found at ' + host);
+    put_markers(map, remotes);
+  });
 }
 
 function put_markers(map, remotes) {
@@ -64,35 +76,24 @@ function put_markers(map, remotes) {
   });
 }
 
-function refresh_list(map) {
-  $.getJSON('http://b1.zold.io/remotes', function(data) {
-    var remotes = data['all'];
-    console.log(remotes.length + ' remote nodes found');
-    put_markers(map, remotes);
-  });
+function put_marker_by_host(map, coords, host, port) {
+  $.getJSON('https://api.exana.io/dns/' + host + '/a', function(json) {
+    ip = $.grep(json['answer'], function (a, i) { return a['type'] == 'A'; })[0]['rdata'];
+    console.log('Host ' + host + ' resolved to ' + ip);
+    put_marker_by_ip(map, coords, ip, port);
+  }).fail(function() { console.log('Failed to find IP for ' + host) });
 }
 
-function refresh(map) {
-  $.getJSON('http://b2.zold.io:4096/', function(data) {
-    $('#header').html(
-      'Version: ' + data['version'] + '<br/>' +
-      'Host: ' + data['score']['host'] + ':' + data['score']['port'] + '<br/>' +
-      'Score: ' + data['score']['value'] + '<br/>' +
-      'Wallets: ' + data['wallets']
-    );
-    refresh_list(map);
-    window.setTimeout(refresh, 10000);
-  });
+function put_marker_by_ip(map, coords, ip, port) {
+  $.getJSON('http://www.geoplugin.net/json.gp?ip=' + ip, function(json) {
+    var lat = parseFloat(json['geoplugin_latitude']), lon = parseFloat(json['geoplugin_longitude']);
+    console.log(ip + ' located at ' + lat + '/' + lon);
+    new google.maps.Marker({
+      position: { lat: lat, lng: lon },
+      map: map,
+      title: coords
+    });
+    console.log('Marker set for ' + coords + ' at ' + lat + '/' + lon);
+  }).fail(function() { console.log('Failed to find geo-location for ' + ip) });
 }
 
-function init() {
-  var map = new google.maps.Map(
-    document.getElementById("map"),
-    {
-      center: new google.maps.LatLng(55.751244, 37.618423),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoom: 3
-    }
-  );
-  refresh(map);
-};
