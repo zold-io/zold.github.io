d198e86e1f54e449f2b8fd7fa0bf9039636e37c2
cdenaro <carlo.denaro@lastminute.com>
2018-06-26 16:35:57 +0200

    better for increase implicit intent

--- js/map.js
+++ js/map.js
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-var currentHashing = {};
+var currentHashing = new Date();
 
 function init() {
   startLoader();
