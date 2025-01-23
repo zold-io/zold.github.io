92afbe13eb9565d1d1d026ca11c00b019332d85e
davvd <davvd@zold.io>
2025-01-23 12:19:11 +0300

    year set to 2025

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /**
 (The MIT License)
 
-Copyright (c) 2018-2019 Zerocracy, Inc.
+Copyright (c) 2018-2025 Zerocracy, Inc.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the 'Software'), to deal
